900a35c08dd3f31a462cc62a178001ef681575db
Denis Sidorenko <dongor7@gmail.com>
2018-09-12 11:47:31 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+function solveEquation(equation) {
+    let variables = equation.split(' ');
+    let a = variables[0];
+    let b = variables[3] === '+' ? variables[4] : -1 * variables[4];
+    let c = variables[7] === '+' ? variables[8] : -1 * variables[8];
+
+    let d = b * b - 4 * a * c;
+
+    let result = [];
+
+    result.push( Math.round(( - b - Math.sqrt( d ) ) / ( 2 * a)) );
+    result.push( Math.round(( - b + Math.sqrt( d ) ) / ( 2 * a)) );
+
+    return result
 }
+
+console.log(solveEquation('30 * x^2 + 63436590 * x - 56700499682640'));
+
+module.exports = solveEquation;
